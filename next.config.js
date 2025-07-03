eb001b11b6525e6ee945011af709c37943ae562d
gladishd <gladish.dean@gmail.com>
2025-07-03 18:52:46 -0500

    drop any trace?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
+  // drop *any* trace of public/scripts/ethernet from all server bundles
   outputFileTracingExcludes: {
-    '**/*': [path.join(__dirname, 'public/scripts/ethernet')],
+    '**/*': [path.join(__dirname, 'public', 'scripts', 'ethernet')],
   },
 }
